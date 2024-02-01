b41017c009845f6c5a0081bac6ae6b267eab1c48
richard246 <107892052+richard246@users.noreply.github.com>
2024-01-31 20:50:02 -0500

    user sign in and list implementations

--- grocerylist/lib/main.dart
+++ grocerylist/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:grocerylist/pages/adduser_page.dart';
-import 'package:grocerylist/pages/createaccount_page.dart';
-import 'package:grocerylist/pages/createlist_page.dart';
-import 'package:grocerylist/pages/login_page.dart';
-import 'package:grocerylist/pages/updatelist_page.dart';
-import 'package:grocerylist/pages/viewlist_page.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:grocerylist/pages/login_page.dart';
+import 'package:grocerylist/pages/viewlist_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +15,12 @@ Future main() async {
             messagingSenderId: "795193426475",
             projectId: "grocerylists-f8cb5"));
   }
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCj1NdSOp6it6yjEfdhe3CbHBcpFeext5k",
+          appId: "1:795193426475:web:816bb399d6780c98bde06e",
+          messagingSenderId: "795193426475",
+          projectId: "grocerylists-f8cb5"));
   runApp(MyApp());
 }
 
@@ -29,7 +29,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Grocery List',
-      home: CreateAccount(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            // Get the user
+            User? user = snapshot.data;
+
+            // If the user is null, they are not logged in; show LoginPage
+            if (user == null) {
+              return LoginPage();
+            }
+
+            // If the user is not null, they are logged in; show ViewListsPage
+            return ViewListsPage();
+          }
+
+          // Show a loading indicator while waiting for the authentication state
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        },
+      ),
     );
   }
 }
--- grocerylist/lib/pages/createlist_page.dart
+++ grocerylist/lib/pages/createlist_page.dart
@@ -8,7 +8,7 @@ import 'package:grocerylist/pages/viewlist_page.dart';
 
 class CreateList extends StatelessWidget {
   const CreateList({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
